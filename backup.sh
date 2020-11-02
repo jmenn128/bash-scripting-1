491fb946a21654bce122fcd6daaf5bc9c4c798d4
jmenn128 <71101666+jmenn128@users.noreply.github.com>
2020-11-01 19:52:34 -0600

    Update backup.sh
    Jared's review of the backup.sh code.. I was not able to find the While block, for block, or the until block.. Only the if-else blocks. The code was not completed to work with multiple directories. However, It does work with a SINGLE directory.

--- backup.sh
+++ backup.sh
@@ -2,6 +2,10 @@
 
 # This bash script is used to backup a user's home directory to /tmp/.
 
+#An if-else statement is used in a situation of condition. For example, if the first condition
+# equals true then the first statement will execute. If the first fails then the second will execute (else).
+
+#backup function not found.. This function was supposed to be paired with the for loop to backup all directories supplied.
 if [ -z $1 ]; then                                                                                                                                                                                                                                                             
         user=$(whoami)                                                                                                                                                                                                                                                         
 else                                                                                                                                                                                                                                                                           
@@ -11,7 +15,8 @@ else
         fi                     
         user=$1                                                                                                                                                                                                 
 fi
-
+#After analyzing the if-else block you can see if a directory exists the first if will be true. If it doesn't
+#the else will execute; echoing the statement above.
 Users/$user/OneDrive/Desktop/UTSA
 output=/c/Users/${user}/OneDrive/Desktop/UTSA_backup_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
@@ -51,3 +56,8 @@ if [ $src_files -eq $arch_files ]; then
 else
         echo "Backup of $input failed!"
 fi
+#for loop not found..
+#The while and until loops were also not available for review.
+#With the above loops not being found I will just provide my general insight of the code
+#I can see that he had issues with directories which is most likely why the above loops/functions were
+#not implemented. However, the above code should work with the use of a SINGLE directory but not multiple.
